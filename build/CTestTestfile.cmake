ed036cd01341bd1fdc4d19b23a44b73ffcd46704
Victor <victor@plc-host.local>
2026-08-07 00:31:52 +0800

    fix: remove build/ from git tracking to prevent CI cross-platform cache errors

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/victor/c_proj/plc_host
-# Build directory: /home/victor/c_proj/plc_host/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("src")
-subdirs("tests")
